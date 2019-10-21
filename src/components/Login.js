b1ec2da849e9cb34d5d05d3efbba8e752117688d
Mildred Awuor <otieno.awuor.m@gmail.com>
2019-10-21 15:42:19 +0300

    added validation to login form

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,32 +1,63 @@
 import React, {useState} from "react";
 import {connect} from "react-redux";
 import * as actionCreators from "../state/actionCreators";
+import { Formik } from 'formik';
+
+
+
+
 
  export const Login = (props) => {
-    const [userDetails, setUserDetails] = useState({username: "", password: ""});
-console.log("props from redux Store", props);
-    const valueChange = (e) => {
-        setUserDetails({...userDetails, [e.target.name]: e.target.value});
-        console.log("form values login", userDetails);
-    }
-    const onLogin = (e) => {
-        e.preventDefault();
-        props.onLogin(userDetails);
-    };
-
+    const [userDetails] = useState({username: "", password: ""});
+    console.log("props from redux Store", props);
     return (
-        <div>
-            <form>
+        <div className="login-form">
+             <Formik
+      initialValues={userDetails}
+      validate={userDetails => {
+        let errors = {};
+        if (!userDetails.username) {
+          errors.username = 'Please provide a username!';
+        } 
+        if (!userDetails.password) {
+            errors.password = 'Please provide a password!';
+          } else if (userDetails.password.length < 7) {
+            errors.password = 'Your password must be more than 8 characters!';
+          }
+          else if (userDetails.password.length > 11) {
+            errors.password = 'Your password must be less than 12 characters!';
+          }
+        return errors;
+      }}
+      onSubmit={(userDetails,actions) => {
+          props.onLogin(userDetails);
+          actions.resetForm();
+      }
+      }
+    >
+      {({
+        values,
+        errors,
+        touched,
+        handleChange,
+        handleSubmit,
+        isSubmitting,
+      }) => (
+          <form onSubmit={handleSubmit}>
                 <label>
                     Username
                 </label>
-                <input value={userDetails.username} type="text" name="username" onChange={valueChange}/>
+                <input value={values.username} type="text" name="username" onChange={handleChange} />
+               <span style={{color: "red"}}> {errors.username && touched.username && errors.username}</span>
                 <label>
                     Password
                 </label>
-                <input value={userDetails.password} type="password" name="password" onChange={valueChange}/>  
-                <button onClick={onLogin}>Submit</button>              
-            </form>
+                <input value={values.password} type="password" name="password" onChange={handleChange} />  
+               <span style={{color: "red"}}> {errors.password && touched.password && errors.password}</span>
+                <button type="submit" disabled={isSubmitting}>Submit</button>              
+            </form> 
+      )}
+    </Formik>
         </div>
     )
 }
