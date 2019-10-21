486b226f9bc93538a3beb0e59cad9b12e364db20
Sandrava Philips <lacasandra1@gmail.com>
2019-10-21 11:40:40 +0100

    built the jsx for the register page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav>
+        <img />
+        <a href="Login.js">LOGIN</a>
+      </nav>
+      
+      <div>
+      <figure>
+        <img />
+      </figure>
+
+      <section>
+        <h3>We offer centralised management for your event planning needs and allow you meet all your client’s requirements with ease. Fill in your information below to get started.</h3>
+
+        <form>
+          <label>
+            Username
+            <input type='text'  />
+          </label>
+
+          <label>
+            Email Address
+            <input type='text'  />
+          </label>
+
+          <label>
+            Password
+            <input type='password'  />
+          </label>
+
+          <label>
+            Confirm Password
+            <input type='password'  />
+          </label>
+
+          <button>REGISTER</button>
+
+        </form>
+      </section>
+      </div>
     </div>
   );
 }
