c224f0422fceeb14273035359191e012e6de1901
Mildred Awuor <otieno.awuor.m@gmail.com>
2019-10-22 12:30:38 +0300

    started working on dashboard

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,11 +1,15 @@
 import React from "react";
+import user_icon from "../imgs/User_icon.png";
+import {StyledDashboard} from "../styles";
 
 const Dashboard  = (props) => {
     console.log("props from Dashboard", props);
     return (
-        <div>
-            Hello from Dashboard!
-        </div>
+        <StyledDashboard>
+            <div>
+                <img alt="user icon" src={user_icon}/>
+            </div>
+        </StyledDashboard>
     )
 }
 
