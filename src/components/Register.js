e6d40cbcecd538699d79c37b8d478326f5e23791
Mildred Awuor <otieno.awuor.m@gmail.com>
2019-10-22 10:48:32 +0300

    fixed Register form
    Co-authored-by: Sandravaphilips <sandra_philips@outlook.com>

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,112 +1,113 @@
-import React, {useState} from 'react';
+import React, { useState } from "react";
 import { NotificationManager } from "react-notifications";
-import styled from 'styled-components';
+import styled from "styled-components";
 
 import image from "../imgs/register_back.jpg";
 import logo from "../imgs/corporate_logo.png";
-import Axios from 'axios';
-import  Form  from './Form';
+import Axios from "axios";
+import Form from "./Form";
 // import {Route} from 'react-router-dom';
 
 const RegisterStyle = styled.div`
-    
-    nav {
-        display: flex;
-        justify-content: space-between;
-        background-color: #C4C4C4;
-         
-        img {
-            width: 5%;
-        }
-        a {
-            background-color: #090429;
-            text-decoration: none;
-            border-radius: 5px;
-            width: 120px;
-            height: 35px;
-            text-align: center;
-            margin-top: 12px;
-            margin-bottom: 12px;
-            margin-right: 15px;
-            padding-top: 10px;
-            color: white;
-            &:hover {
-                background-color: #FBAB06;
-            }
-        }
+  height: 100vh;
+
+  nav {
+    display: flex;
+    justify-content: space-between;
+    background-color: #c4c4c4;
+
+    img {
+      width: 5%;
     }
-    div {
-        display: flex;
-        align-items: center;
-        background-color: #E5E5E5;
-        
-        img {
-            width: 50%;
-        }
-        section {
-            text-align: center;
-            
-            margin: 0;
-        }
+    a {
+      background-color: #090429;
+      text-decoration: none;
+      border-radius: 5px;
+      width: 120px;
+      height: 35px;
+      text-align: center;
+      margin-top: 12px;
+      margin-bottom: 12px;
+      margin-right: 15px;
+      padding-top: 10px;
+      color: white;
+      &:hover {
+        background-color: #fbab06;
+      }
     }
-`
+  }
+  div {
+    display: flex;
+    align-items: center;
+    background-color: #e5e5e5;
+
+    img {
+      width: 50%;
+      height: 100vh;
+    }
+    section {
+      text-align: center;
+
+      margin: 0;
+    }
+  }
+`;
 
 const initialFormValues = {
-  username: '',
-  emailAddress: '',
-  password: '',
-  confirmPassword: ''
-}
+  username: "",
+  emailAddress: "",
+  password: "",
+  confirmPassword: ""
+};
 
 export default function Register(props) {
   const [formValues, setFormValues] = useState(initialFormValues);
-  const [displayLogin, setDisplayLogin] = useState(false);
-
 
   const onNameChange = e => {
-    setFormValues({...formValues, username: e.target.value})
-  }
+    setFormValues({ ...formValues, username: e.target.value });
+  };
 
   const onEmailChange = e => {
-    setFormValues({...formValues, emailAddress: e.target.value})
-  }
+    setFormValues({ ...formValues, emailAddress: e.target.value });
+  };
 
   const onPasswordChange = e => {
-    setFormValues({...formValues, password: e.target.value})
-  }
+    setFormValues({ ...formValues, password: e.target.value });
+  };
 
   const onCPasswordChange = e => {
-    setFormValues({...formValues, confirmPassword: e.target.value})
-  }
+    setFormValues({ ...formValues, confirmPassword: e.target.value });
+  };
 
   const onFormSubmit = (e, formValues) => {
     e.preventDefault();
-    
-
 
     const newUserDetails = {
       username: formValues.username,
       password: formValues.password,
       id: Date.now()
-    }
+    };
     if (formValues.password === formValues.confirmPassword) {
-      Axios.post('https://cors-anywhere.herokuapp.com/https://corporate-event-planner-build.herokuapp.com/api/auth/register', newUserDetails)
-      .then((response) => {
-        setDisplayLogin(true)
-        NotificationManager.success("Registration successful")
-        props.history.push("/");
-      })
-      .catch(err => {
-        NotificationManager.error(err.message,"Something went terribly wrong!")
-          console.log(err)
+      Axios.post(
+        "https://cors-anywhere.herokuapp.com/https://corporate-event-planner-build.herokuapp.com/api/auth/register",
+        newUserDetails
+      )
+        .then(response => {
+          NotificationManager.success("Registration successful");
+          props.history.push("/");
         })
+        .catch(err => {
+          NotificationManager.error(
+            err.message,
+            "Something went terribly wrong!"
+          );
+          console.log(err);
+        });
     } else {
-      setFormValues({...formValues, password: '', confirmPassword: ''});
-      alert('password fields must be the same!!')
+      setFormValues({ ...formValues, password: "", confirmPassword: "" });
+      alert("password fields must be the same!!");
     }
-
-
-  }
+  };
 
   return (
     <RegisterStyle>
@@ -116,22 +117,25 @@ export default function Register(props) {
       </nav>
 
       <div>
+        <img src={image} alt="for-register" />
 
-        <img src={image} alt='for-register' />
+        <section>
+          <h3>
+            We offer centralised management for your event planning needs and
+            allow you meet all your client’s requirements with ease. Fill in
+            your information below to get started.
+          </h3>
 
-
-      <section>
-        <h3>We offer centralised management for your event planning needs and allow you meet all your client’s requirements with ease. Fill in your information below to get started.</h3>
-
-        <Form onNameChange={onNameChange} onFormSubmit={onFormSubmit} onEmailChange={onEmailChange} onCPasswordChange={onCPasswordChange} formValues={formValues} onPasswordChange={onPasswordChange} />
-      </section>
+          <Form
+            onNameChange={onNameChange}
+            onFormSubmit={onFormSubmit}
+            onEmailChange={onEmailChange}
+            onCPasswordChange={onCPasswordChange}
+            formValues={formValues}
+            onPasswordChange={onPasswordChange}
+          />
+        </section>
       </div>
-
-      {/* {
-        displayLogin && (
-          // <Route exact path='/login' component={Login} />
-        )
-      } */}
     </RegisterStyle>
   );
-} 
+}
