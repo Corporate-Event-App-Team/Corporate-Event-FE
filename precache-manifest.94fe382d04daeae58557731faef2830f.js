e8206a7cde353f232aaa24d89bb2a4d67a24149c
Mildred Awuor <otieno.awuor.m@gmail.com>
2019-10-23 15:28:51 +0300

    Updates

---
+++ precache-manifest.94fe382d04daeae58557731faef2830f.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0711a0ddf4504fecbe5d98d3428e41f",
+    "url": "/Corporate-Event-FE/index.html"
+  },
+  {
+    "revision": "ce85fcc9ee0004fc70b5",
+    "url": "/Corporate-Event-FE/static/css/main.17073c00.chunk.css"
+  },
+  {
+    "revision": "4752f0891e1ea3ce0ee1",
+    "url": "/Corporate-Event-FE/static/js/2.6a605bcb.chunk.js"
+  },
+  {
+    "revision": "ce85fcc9ee0004fc70b5",
+    "url": "/Corporate-Event-FE/static/js/main.5249b91f.chunk.js"
+  },
+  {
+    "revision": "64facf3e5610e04b9434",
+    "url": "/Corporate-Event-FE/static/js/runtime-main.969ec293.js"
+  },
+  {
+    "revision": "ad152469f3739bbb91de455988bcab4b",
+    "url": "/Corporate-Event-FE/static/media/form_back.ad152469.jpeg"
+  },
+  {
+    "revision": "e3c353775509c1073704519edd562973",
+    "url": "/Corporate-Event-FE/static/media/login_back.e3c35377.jpg"
+  },
+  {
+    "revision": "cf3e77b5a63b53d2e9cb61eabc155e15",
+    "url": "/Corporate-Event-FE/static/media/pass_icon_2.cf3e77b5.jpg"
+  },
+  {
+    "revision": "e7119e2a3b3a15136309c2100dd82ceb",
+    "url": "/Corporate-Event-FE/static/media/user_icon_2.e7119e2a.png"
+  },
+  {
+    "revision": "f7f78085829afc7495526efc316fb3f4",
+    "url": "/Corporate-Event-FE/static/media/username_icon.f7f78085.jpg"
+  }
+]);
